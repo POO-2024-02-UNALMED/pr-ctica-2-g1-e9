81c880a9a8b7f3c45f3a84e503b47e4f8467ff84
juanleonardo10 <159485131+juanleonardo10@users.noreply.github.com>
2025-02-10 03:24:24 +0000

    Guia

---
+++ src/Sala.py
@@ -0,0 +1,26 @@
+class Sala:
+    def __init__(self, biblioteca, nombre, capacidad):
+        self.biblioteca = biblioteca
+        self.nombre = nombre
+        self.capacidad = capacidad
+
+    def get_nombre(self):
+        return self.nombre
+
+    def get_capacidad(self):
+        return self.capacidad
+
+    def get_biblioteca(self):
+        return self.biblioteca
+
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+
+    def set_capacidad(self, capacidad):
+        self.capacidad = capacidad
+
+    def set_biblioteca(self, biblioteca):
+        self.biblioteca = biblioteca
+
+    def __str__(self):
+        return self.nombre
